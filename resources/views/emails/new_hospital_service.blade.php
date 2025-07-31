b55f81595fa768bac48e82a5f62263871966da06
alaaalzoubi1 <alaaalzoubu321@gmail.com>
2025-08-01 02:26:54 +0300

    fixing email issues

--- resources/views/emails/new_hospital_service.blade.php
+++ resources/views/emails/new_hospital_service.blade.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>New Service Added</title>
 </head>
 <body>
-    <h1>New Service Added by {{ $hospitalName }}</h1>
+<h1>New Service Added by {{ $hospitalName }}</h1>
 <p>A new service has been added to the hospital:</p>
 <p><strong>Service Name:</strong> {{ $serviceName }}</p>
 <p>Please review the new service addition.</p>
