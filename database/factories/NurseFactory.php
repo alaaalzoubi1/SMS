551b14dccff8ebe9170e6fa260b503fde3a3dc82
alaaalzoubi1 <alaaalzoubu321@gmail.com>
2025-08-07 02:27:04 +0300

    testing database 3

--- database/factories/NurseFactory.php
+++ database/factories/NurseFactory.php
@@ -25,7 +25,7 @@ class NurseFactory extends Factory
             'full_name' => $this->faker->name,
             'address' => $this->faker->address,
             'graduation_type' => $this->faker->randomElement(['معهد', 'مدرسة', 'جامعة', 'ماجستير', 'دكتوراه']),
-            'location' => new Point(lat: $this->faker->latitude(),lng: $this->faker->longitude(),srid: 4326 ),
+            'location' => new Point(lat: min(max($this->faker->latitude(), -90), 90),lng: min(max($this->faker->longitude(), -180), 180),srid: 4326 ),
             'age' => $this->faker->numberBetween(25, 65),
             'gender' => $this->faker->randomElement(['male', 'female']),
             'profile_description' => $this->faker->sentence,
