9c2b3025957d54eb411895d3d29f957b7a5b6771
alaaalzoubi1 <alaaalzoubu321@gmail.com>
2025-10-08 00:46:18 +0300

    hospital reservations bugs

--- app/Models/Hospital.php
+++ app/Models/Hospital.php
@@ -25,8 +25,8 @@ class Hospital extends Model
         'unique_code',
     ];
     protected $casts = [
-        'created_at' => 'datetime',
-        'updated_at' => 'datetime',
+        'created_at' => 'date',
+        'updated_at' => 'date',
     ];
 
     public function account():BelongsTo
