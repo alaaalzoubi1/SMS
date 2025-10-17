f02e3c6b3615e34c5e07aa42c34f0990b921548a
alaaalzoubi1 <alaaalzoubu321@gmail.com>
2025-10-17 16:10:02 +0300

    doctor seeder

--- database/seeders/SpecializationSeeder.php
+++ database/seeders/SpecializationSeeder.php
@@ -2,21 +2,43 @@
 
 namespace Database\Seeders;
 
-use App\Models\Specialization;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Specialization;
 
 class SpecializationSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        Specialization::insert([
-            ['id' => 1, 'name' => 'طب عام'],
-            ['id' => 2, 'name' => 'طب أسنان'],
-            ['id' => 3, 'name' => 'جلدية'],
-        ]);
+        $specializations = [
+            [
+                'name_en' => 'General Medicine',
+                'name_ar' => 'طب عام',
+                'image'   => 'specializations/general.png',
+            ],
+            [
+                'name_en' => 'Dentistry',
+                'name_ar' => 'طب أسنان',
+                'image'   => 'specializations/dentistry.png',
+            ],
+            [
+                'name_en' => 'Dermatology',
+                'name_ar' => 'جلدية',
+                'image'   => 'specializations/dermatology.png',
+            ],
+            [
+                'name_en' => 'Pediatrics',
+                'name_ar' => 'طب أطفال',
+                'image'   => 'specializations/pediatrics.png',
+            ],
+            [
+                'name_en' => 'Ophthalmology',
+                'name_ar' => 'طب عيون',
+                'image'   => 'specializations/ophthalmology.png',
+            ],
+        ];
+
+        foreach ($specializations as $specialization) {
+            Specialization::create($specialization);
+        }
     }
 }
