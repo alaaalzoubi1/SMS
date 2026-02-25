1a1053cb546bd59aba8aa177be455f88044072b9
alaaalzoubi1 <alaaalzoubu321@gmail.com>
2026-02-26 01:26:52 +0300

    fixing the job

--- app/Jobs/SendFirebaseNotificationJob.php
+++ app/Jobs/SendFirebaseNotificationJob.php
@@ -3,6 +3,7 @@
 namespace App\Jobs;
 
 use App\Services\FirebaseService;
+use Illuminate\Bus\Batchable;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -11,7 +12,7 @@ use Illuminate\Queue\SerializesModels;
 
 class SendFirebaseNotificationJob implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels,Batchable;
 
     protected string $token;
     protected string $title;
