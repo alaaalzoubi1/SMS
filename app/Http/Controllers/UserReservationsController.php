62f70bd884281d9f6954d0433a3d64907c7488a9
alaaalzoubi1 <alaaalzoubu321@gmail.com>
2026-02-28 19:55:43 +0300

    new service system for the nurse

--- app/Http/Controllers/UserReservationsController.php
+++ app/Http/Controllers/UserReservationsController.php
@@ -37,7 +37,7 @@ class UserReservationsController extends Controller
             case 'nurse':
                 $reservations = \App\Models\NurseReservation::query()
                     ->where('user_id', $userId)
-                    ->with(['nurse','nurse.account:id,phone_number', 'nurseService:id,name','cancellation'])
+                    ->with(['nurse','nurse.account:id,phone_number', 'nurseService.service','cancellation'])
                     ->when($apiStatus, function ($q) use ($statusMap, $apiStatus) {
                         $status = $statusMap[$apiStatus]['nurse'] ?? null;
                         if ($status) $q->where('status', $status);
@@ -82,7 +82,7 @@ class UserReservationsController extends Controller
             default:
                 $user = \App\Models\User::query()
                     ->with([
-                        'nurseReservations' => fn($q) => $q->with(['nurse','nurseService:id,name','cancellation'])->orderByDesc('created_at'),
+                        'nurseReservations' => fn($q) => $q->with(['nurse','nurseService.service','cancellation'])->orderByDesc('created_at'),
                         'hospitalReservations' => fn($q) => $q->with(['hospital','hospitalService.service','cancellation'])->orderByDesc('created_at'),
                         'doctorReservations' => fn($q) => $q->with(['doctor.specialization','doctorService','cancellation'])->orderByDesc('created_at'),
                     ])
